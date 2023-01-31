ad6eb6dfbdfbd5425753af3815c27b5575aaef8b
AlpacaRex <1030519668@qq.com>
2023-01-31 14:16:54 +0800

    migrate

--- vote/views.py
+++ vote/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class LoginView(APIView):
-    def get(self, request):
+    def post(self, request):
         code = request.data.get('code')
-        print(code)
+
         return Response({'msg': 'nmsl'})
 
