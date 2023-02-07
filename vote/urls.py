fe2f7d8adc36cb5584c755903cd8bcfe6739e2d4
AlpacaRex <1030519668@qq.com>
2023-02-07 14:40:51 +0800

    add DeleteVotingView

--- vote/urls.py
+++ vote/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from vote.views import UserView, VotingView, VotingItemView, QRCodeView
+from vote.views import UserView, VotingView, VotingItemView, QRCodeView, DeleteVotingView
 
 router = routers.DefaultRouter()
 router.register('voting', VotingView, basename='voting')
@@ -8,7 +8,8 @@ router.register('votingItem', VotingItemView, basename='votingItem')
 
 urlpatterns = [
     path('user/', UserView.as_view()),
-    path('qrcode/', QRCodeView.as_view())
+    path('qrcode/', QRCodeView.as_view()),
+    path('deleteVoting/', DeleteVotingView.as_view()),
 ]
 
 urlpatterns += router.urls
